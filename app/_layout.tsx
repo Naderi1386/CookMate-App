902b7f91093701bed09e6368ed17162e3e8b1a2b
unknown <mh.naderi1386@gmail.com>
2025-07-10 15:57:02 +0330

    added Stack.Navigator and Stack.Screens components

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,20 @@
-import { View } from "react-native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StackParamList } from "@/types/Stack";
+import Welcome from "@/screens/Welcome";
+import Home from "@/screens/Home";
+import FoodDetails from "@/screens/FoodDetails";
+
+const Stack = createNativeStackNavigator<StackParamList>();
 
 export default function RootLayout() {
-  return <View></View>;
+  return (
+    <Stack.Navigator
+      screenOptions={{ headerShown: false }}
+      initialRouteName="Welcome"
+    >
+      <Stack.Screen name="Welcome" component={Welcome} />
+      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="FoodDetails" component={FoodDetails} />
+    </Stack.Navigator>
+  );
 }
---
+++ types/Stack.ts
@@ -0,0 +1,5 @@
+export type StackParamList = {
+  Welcome: undefined;
+  Home: undefined;
+  FoodDetails: undefined;
+};
