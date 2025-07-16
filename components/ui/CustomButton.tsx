8957363ac060cc5bc09cf6798937d22138a35b48
unknown <mh.naderi1386@gmail.com>
2025-07-17 00:36:20 +0330

    added some components in Welcome screen and change ui and added a CustomButton component

---
+++ components/ui/CustomButton.tsx
@@ -0,0 +1,36 @@
+import { Colors } from "@/constants/Colors";
+import { FontSize } from "@/constants/FontSize";
+import { Button } from "@rneui/themed";
+import { ReactNode } from "react";
+import { StyleSheet, Text } from "react-native";
+
+const CustomButton = ({
+  children,
+  onPress,
+}: {
+  children: ReactNode;
+  onPress?: () => void;
+}) => {
+  return (
+    <Button onPress={onPress} buttonStyle={Styles.button}>
+      <Text style={Styles.text}>{children}</Text>
+    </Button>
+  );
+};
+
+export default CustomButton;
+
+const Styles = StyleSheet.create({
+  button: {
+    backgroundColor: Colors.main,
+    borderRadius:8,
+    paddingBottom:14,
+    paddingHorizontal:16
+  },
+  text: {
+    color: Colors.white,
+    fontSize:FontSize.lg,
+    fontWeight:"600"
+    
+  },
+});
--- screens/Welcome.tsx
+++ screens/Welcome.tsx
@@ -1,14 +1,65 @@
+import CustomButton from "@/components/ui/CustomButton";
 import Layout from "@/components/ui/Layout";
-import { Text, View } from "react-native";
+import { FontSize } from "@/constants/FontSize";
+import { Image, StyleSheet, Text, View } from "react-native";
 
 const Welcome = () => {
   return (
     <Layout>
-      <View>
-        <Text>hello welcome</Text>
+      <View style={Styles.container}>
+        <Image
+          style={Styles.logo}
+          source={{
+            uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSCezZAKy4aJLPVlLu1KoyAcbnU_KNUjxIew&s",
+          }}
+        />
+        <Text style={Styles.title}>🍽️ Welcome to AshpazBashi!</Text>
+        <Text style={Styles.text}>
+          🥘 Thousands of delicious recipes from around the world — ready to
+          turn you into a home chef! 👨‍🍳 With just a few taps, start cooking your
+          favorite meals — easy, fast, and step-by-step.
+        </Text>
+        <Text style={Styles.text}>
+          🔎 Discover, learn, and get inspired... ❤️ Save your favorite dishes
+          and come back anytime!
+        </Text>
+        <Text style={Styles.text}>
+          🍳 Ready to cook your first recipe? 👉 Tap “Get Started” and dive into
+          the world of flavors!
+        </Text>
+        <View>
+          <CustomButton>start</CustomButton>
+        </View>
       </View>
     </Layout>
   );
 };
 
 export default Welcome;
+
+const Styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    gap: 8,
+    alignItems: "center",
+    paddingVertical: 10,
+  },
+  logo: {
+    width: 90,
+    height: 90,
+    resizeMode: "contain",
+  },
+  title: {
+    fontWeight: "600",
+    fontSize: FontSize["2xl"],
+    marginBottom: 16,
+  },
+  text: {
+    fontWeight: "500",
+    fontSize: FontSize.md,
+    marginBottom: 10,
+    textAlign: "center",
+  },
+});
