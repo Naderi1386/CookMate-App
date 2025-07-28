1f10aff9937da56c5ebd8d9b1c803599a1b4ae9e
unknown <mh.naderi1386@gmail.com>
2025-07-28 19:13:46 +0330

    added persist for our store

--- store/store.ts
+++ store/store.ts
@@ -1,5 +1,7 @@
 import { RecipesType } from "@/types/Recipes";
 import { create } from "zustand";
+// import { persist } from "zustand/middleware";
+// import AsyncStorage from "@react-native-async-storage/async-storage";
 
 type StoreType = {
   recipes: RecipesType[];
@@ -16,3 +18,34 @@ export const useStore = create<StoreType>()((set) => ({
       recipes: state.recipes.filter((recipe) => recipe.id !== id),
     })),
 }));
+
+/*
+  export const useStore = create<StoreType>()(
+    persist(
+      (set) => ({
+        recipes: [],
+        addRecipe: (recipe) =>
+          set((state) => ({ recipes: [...state.recipes, recipe] })),
+        removeRecipe: (id) =>
+          set((state) => ({
+            recipes: state.recipes.filter((recipe) => recipe.id !== id),
+          })),
+      }),
+      {
+        name: "recipes-favorites-storage",
+        storage: {
+          getItem: async (name) => {
+            const item = await AsyncStorage.getItem(name);
+            return item ? JSON.parse(item) : null;
+          },
+          setItem: async (name, value) => {
+            await AsyncStorage.setItem(name, JSON.stringify(value));
+          },
+          removeItem: async (name) => {
+            await AsyncStorage.removeItem(name);
+          },
+        },
+      }
+    )
+  );
+*/
