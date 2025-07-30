75d38fc3c4c7919b8a55b2b7248e9f10532f96dc
unknown <mh.naderi1386@gmail.com>
2025-07-30 12:15:19 +0330

    added a new feature for Menu component

--- components/ui/Menu.tsx
+++ components/ui/Menu.tsx
@@ -1,8 +1,18 @@
 import { Colors } from "@/constants/Colors";
-import { Dispatch, SetStateAction } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Dispatch, SetStateAction, useEffect, useRef, useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Animated,
+  GestureResponderEvent,
+} from "react-native";
 import Logo from "./Logo";
 import { FontSize } from "@/constants/FontSize";
+import { Ionicons } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
+import { NavigationProp } from "@/types/Navigation";
+import { useOutsideTap } from "@/hooks/useOutsideClick";
 
 const Menu = ({
   isShow,
@@ -11,38 +21,99 @@ const Menu = ({
   isShow: boolean;
   setIsShow: Dispatch<SetStateAction<boolean>>;
 }) => {
-  if (!isShow) return null;
+  const fadeAnim = useRef(new Animated.Value(0)).current;
+  const [visible, setVisible] = useState(isShow);
+  const navigation = useNavigation<NavigationProp>();
+
+  // اینجا از هوک استفاده کردیم
+  const { ref, handlePress } = useOutsideTap(() => setIsShow(false));
+
+  useEffect(() => {
+    if (isShow) {
+      setVisible(true);
+      Animated.timing(fadeAnim, {
+        toValue: 1,
+        duration: 400,
+        useNativeDriver: true,
+      }).start();
+    } else {
+      Animated.timing(fadeAnim, {
+        toValue: 0,
+        duration: 400,
+        useNativeDriver: true,
+      }).start(() => {
+        setVisible(false);
+      });
+    }
+  }, [isShow, fadeAnim]);
+
+  if (!visible) return null;
+
+  const onRelease = (evt: GestureResponderEvent) => {
+    handlePress(evt);
+    return false;
+  };
+
   return (
-    <View style={Styles.container}>
-      <View style={Styles.logoWrraper}>
-        <Logo height={55} width={55} />
+    <Animated.View
+      style={[Styles.overlay, { opacity: fadeAnim }]}
+      onStartShouldSetResponder={() => true}
+      onResponderRelease={onRelease}
+    >
+      <View ref={ref} style={Styles.container}>
+        <View style={Styles.logoWrraper}>
+          <Logo height={55} width={55} />
+        </View>
+        <View style={Styles.mainWrraper}>
+          <Text style={Styles.title}>Cook Mate 👩‍🍳</Text>
+          <View style={Styles.list}>
+            <View style={Styles.listItem}>
+              <Ionicons name="home-outline" size={24} color={Colors.main} />
+              <Text
+                style={Styles.listItemTitle}
+                onPress={() => navigation.navigate("Home")}
+              >
+                Home
+              </Text>
+            </View>
+            <View style={Styles.listItem}>
+              <Ionicons name="heart-outline" size={24} color={Colors.main} />
+              <Text
+                style={Styles.listItemTitle}
+                onPress={() => navigation.navigate("Favorites")}
+              >
+                Favorites
+              </Text>
+            </View>
+          </View>
+        </View>
       </View>
-      <View style={Styles.mainWrraper}>
-        <Text style={Styles.title}>Cook Mate 👩‍🍳</Text>
-      </View>
-    </View>
+    </Animated.View>
   );
 };
 
 export default Menu;
 
 const Styles = StyleSheet.create({
-  container: {
+  overlay: {
     flex: 1,
     position: "fixed",
-    zIndex: 10000,
     left: 0,
-    bottom: 0,
+    right: 0,
     top: 0,
-    width: 240,
-
-    paddingVertical: 6,
+    bottom: 0,
+    zIndex: 10000,
+    backgroundColor: "rgba(0,0,0,0.2)",
+  },
+  container: {
+    width: 265,
+    height: "100%",
     backgroundColor: Colors.white,
     borderRightWidth: 4,
     borderColor: Colors.sub,
+    paddingVertical: 6,
   },
   logoWrraper: {
-    display: "flex",
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "center",
@@ -60,5 +131,19 @@ const Styles = StyleSheet.create({
     textAlign: "center",
     paddingTop: 8,
     paddingBottom: 14,
+  },
+  list: {
+    flexDirection: "column",
+    gap: 20,
+    marginTop: 12,
+  },
+  listItem: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 8,
+  },
+  listItemTitle: {
+    fontSize: FontSize.lg,
+    fontWeight: "600",
   },
 });
